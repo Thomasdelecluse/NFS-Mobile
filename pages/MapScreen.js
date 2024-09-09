da54eef9989a0287e7f3c7bcb6bb2fce7ab8b116
Thomasdelecluse <81568452+Thomasdelecluse@users.noreply.github.com>
2024-09-09 22:45:20 +0200

    feat: Map Screen v1

---
+++ pages/MapScreen.js
@@ -0,0 +1,13 @@
+// ProfileScreen.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MapScreen = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor:'#12CBC4' }}>
+            <Text>Map Screen</Text>
+        </View>
+    );
+};
+
+export default MapScreen;
