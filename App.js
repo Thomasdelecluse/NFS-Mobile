5d1bac9c89f24b795337bf2048a5c72ba6ca93ea
Thomasdelecluse <81568452+Thomasdelecluse@users.noreply.github.com>
2024-09-10 09:16:29 +0200

    feat: add app.js

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet} from 'react-native';
+import BottomTabNavigator from "./component/BottomTabNavigator";
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <BottomTabNavigator />;
 }
 
 const styles = StyleSheet.create({
