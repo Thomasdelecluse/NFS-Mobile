9415ec91e7ee2d5ea99b7ed6db84f76b9a827a51
Thomasdelecluse <81568452+Thomasdelecluse@users.noreply.github.com>
2024-09-11 22:24:47 +0200

    feat: create dao

--- dao/EventDAO.js
+++ dao/EventDAO.js
@@ -1,12 +1,6 @@
 import axios from 'axios';
 
+const url = "https://fesipopapi.onrender.com/evenements"
+export const getDataFromAPI = () => axios.get(url).then(res => res.data);
 
-export const getDataFromAPI = async () => {
-    try {
-        const response = await axios.get(''); // Remplace par l'URL de ton API
-        return response.data; // Retourner les données
-    } catch (error) {
-        console.error('Erreur lors de la récupération des données :', error.response ? error.response.data : error.message);
-        throw error; // Propager l'erreur pour gestion dans le composant appelant
-    }
-};
+
